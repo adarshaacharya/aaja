d47b68c4a07c9a55398c71176318f1501f596ee4
maxiums <connectwithadarsha@gmail.com>
2021-05-16 18:37:34 +0545

    add dns checkup

--- bin/index.js
+++ bin/index.js
@@ -15,24 +15,26 @@ updateNotifier({ pkg }).notify();
 
 const spinner = ora();
 const arg = process.argv[2];
-const url = `https://www.hamropatro.com/`;
+const url = `hamropatro.com`;
 
-if (arg === '-h' || arg === '--help') {
+
+if ( arg === '-h' || arg === '--help') {
   console.log(`
-  ${chalk.cyan('Usage:')} 
-
-  ${chalk.cyan('$ aaja')}     display today's nepali date
-`);
+    ${chalk.cyan('Usage:')} 
+  
+    ${chalk.cyan('$ aaja')}     display today's nepali date
+  `);
   process.exit(1);
 }
 
 dns.lookup(url, (err) => {
   if (err) {
+    console.log(err)
     logUpdate(`\n ✖ You're offline! \n`);
-   process.exit(1)
+    process.exit(1);
   } else {
     logUpdate();
-    spinner.text = `Fetching today's date`
+    spinner.text = `Fetching today's date`;
     spinner.start();
   }
 });
