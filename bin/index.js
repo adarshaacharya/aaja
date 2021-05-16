130fbcb1ff9493e337c0b777d97646a162296117
maxiums <connectwithadarsha@gmail.com>
2021-05-16 23:39:02 +0545

    create aaja object

--- bin/index.js
+++ bin/index.js
@@ -15,10 +15,9 @@ updateNotifier({ pkg }).notify();
 
 const spinner = ora();
 const arg = process.argv[2];
-const url = `hamropatro.com`;
+const url = `english.hamropatro.com`;
 
-
-if ( arg === '-h' || arg === '--help') {
+if (arg === '-h' || arg === '--help') {
   console.log(`
     ${chalk.cyan('Usage:')} 
   
@@ -29,12 +28,59 @@ if ( arg === '-h' || arg === '--help') {
 
 dns.lookup(url, (err) => {
   if (err) {
-    console.log(err)
+    console.log(err);
     logUpdate(`\n ✖ You're offline! \n`);
     process.exit(1);
   } else {
     logUpdate();
-    spinner.text = `Fetching today's date`;
+    spinner.text = `Fetching today's date\n\n`;
     spinner.start();
   }
 });
+
+const showError = () => {
+  logUpdate(`\n${chalk.dim('Could not fetch date. Please try again!')}\n`);
+  process.exit(1);
+};
+
+let aaja = {};
+
+got(`https://${url}`)
+  .then((res) => {
+    const $ = cheerio.load(res.body, {
+      xml: {
+        normalizeWhitespace: true,
+      },
+    });
+
+    const main = `#top .container12 .column4 .logo`;
+
+    const npdate = $(`${main} .date`);
+    aaja.date = npdate.text().trim();
+
+    const [tithi, events] = $(`${main} .events`)
+      .text()
+      .trim()
+      .split(',')
+      .map((el) => el.trim());
+
+    aaja.tithi = tithi;
+    aaja.events = events.split('/');
+
+    const time = $(`${main} .time > span:nth-child(1)`);
+    aaja.time = time.text().trim();
+
+    const ad = $(`${main} .time >span.eng`);
+    aaja.ad = ad.text().trim();
+
+    spinner.stop();
+    console.log(aaja);
+  })
+  .catch((err) => {
+    console.log(err);
+    if (err) showError();
+  });
+
+function escape(str) {
+  return str.replace(/\s/g, ' ').trim();
+}
